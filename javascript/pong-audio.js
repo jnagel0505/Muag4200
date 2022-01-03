5dac891f351573986ccea44f82dbc38426136e3c
jnagel0505 <96092007+jnagel0505@users.noreply.github.com>
2022-01-02 23:37:23 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -63,14 +63,14 @@ scoreSoundRight.player.volume.value = 10
 export var ambientSound = new soundFile("Ambience.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = 0; //turn down volume
+ambientSound.player.volume.value = 20; //turn down volume
 
 export var adventureMusic = new soundFile("Left.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = 0;
+adventureMusic.player.volume.value = -20;
 
 export var villageMusic = new soundFile("Right.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
-villageMusic.player.volume.value = 0;
+villageMusic.player.volume.value = -20;
